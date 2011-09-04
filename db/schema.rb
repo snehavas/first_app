2d54d1e30ad86772093a5dc0c6be89d9fd4e34fc
twer <twu@twu-pc.(none)>
2011-09-04 23:15:59 +0530

    created users and generated script using scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110904173024) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
 end
