ed40d128fd5c86ea6def699e05e6fbdd750bfb2f
twer <twu@twu-pc.(none)>
2011-09-06 23:27:42 +0530

    working for one use

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-   has_many :users
+  has_many :friendships
+  has_many :friends, :through => :friendships
 
 
 end
--- db/migrate/20110904173024_add_user_id_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :users, :user_id
-  end
-end
